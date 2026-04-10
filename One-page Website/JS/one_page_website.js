c34933b4f1e19bae0f734e60ab7a01f7b8cd1bd3
subin <subindasks@gmail.com>
2026-04-10 12:36:33 -0400

    addes showslides

--- One-page Website/JS/one_page_website.js
+++ One-page Website/JS/one_page_website.js
@@ -1,7 +1,31 @@
+let slideIndex = 1;
+
 function openModal() {
 document.getElementById("myModal").style.display = "block";
+showSlides(slideIndex);
 }
 
 function closeModal() {
 document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let slides = document.getElementsByClassName("lightbox-img");
+
+if (n > slides.length) { slideIndex = 1 }
+if (n < 1) { slideIndex = slides.length }
+
+for (let i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+
+slides[slideIndex-1].style.display = "block";
 }
